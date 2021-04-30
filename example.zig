d9997356dd5fb5ce08d9ff36d7717c6ad0cbfda4
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-04-30 12:00:23 +0800

    use GPA and std.log in example

--- example.zig
+++ example.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
+const log = std.log.scoped(.main);
+
 const Linenoise = @import("linenoise").Linenoise;
 
 fn completion(allocator: *Allocator, buf: []const u8) ![][]const u8 {
@@ -24,16 +26,16 @@ fn hints(allocator: *Allocator, buf: []const u8) !?[]const u8 {
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = &arena.allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = &gpa.allocator;
 
     var ln = Linenoise.init(allocator);
     defer ln.deinit();
 
     // Load history and save history later
-    ln.history.load("history.txt") catch std.debug.print("Failed to load history\n", .{});
-    defer ln.history.save("history.txt") catch std.debug.print("Failed to save history\n", .{});
+    ln.history.load("history.txt") catch log.err("Failed to load history", .{});
+    defer ln.history.save("history.txt") catch log.err("Failed to save history", .{});
 
     // Set up hints callback
     ln.hints_callback = hints;
@@ -49,7 +51,7 @@ pub fn main() !void {
 
     while (try ln.linenoise("hellö> ")) |input| {
         defer allocator.free(input);
-        std.debug.print("input: {s}\n", .{input});
+        log.info("input: {s}", .{input});
         try ln.history.add(input);
     }
 }
