0f3f47eef84cfbbb7fc87a4da39dbbd31412445c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2023-05-01 09:36:14 +0200

    Update to latest zig
    zig version 0.11.0-dev.2862+66da64d77

--- build.zig
+++ build.zig
@@ -2,27 +2,20 @@ const Build = @import("std").Build;
 const FileSource = Build.FileSource;
 
 pub fn build(b: *Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const optimize = b.standardOptimizeOption(.{});
 
     const wcwidth = b.dependency("wcwidth", .{
         .target = target,
         .optimize = optimize,
-    });
+    }).module("wcwidth");
 
     const linenoise = b.addModule("linenoise", .{
         .source_file = .{ .path = "src/main.zig" },
         .dependencies = &.{
             .{
                 .name = "wcwidth",
-                .module = wcwidth.module("wcwidth"),
+                .module = wcwidth,
             },
         },
     });
@@ -34,9 +27,9 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib.addModule("wcwidth", wcwidth.module("wcwidth"));
+    lib.addModule("wcwidth", wcwidth);
     lib.linkLibC();
-    lib.install();
+    b.installArtifact(lib);
 
     // Tests
     var main_tests = b.addTest(.{
@@ -45,8 +38,11 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    const run_main_tests = b.addRunArtifact(main_tests);
+
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 
     // Zig example
     var example = b.addExecutable(.{
@@ -57,7 +53,7 @@ pub fn build(b: *Build) void {
     });
     example.addModule("linenoise", linenoise);
 
-    var example_run = example.run();
+    var example_run = b.addRunArtifact(example);
 
     const example_step = b.step("example", "Run example");
     example_step.dependOn(&example_run.step);
@@ -73,7 +69,7 @@ pub fn build(b: *Build) void {
     c_example.linkLibC();
     c_example.linkLibrary(lib);
 
-    var c_example_run = c_example.run();
+    var c_example_run = b.addRunArtifact(c_example);
 
     const c_example_step = b.step("c-example", "Run C example");
     c_example_step.dependOn(&c_example_run.step);
